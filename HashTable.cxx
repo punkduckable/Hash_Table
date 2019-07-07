bb37a434beb2ba634ceafee860cfec4e045a1ea5
Robert Stephany <robert.stephany@utexas.edu>
2019-07-07 15:32:33 -0500

    What I had when I started this repo
    I was originally working on a hash table project on my computer. However, once I realized how much there was to this project, I decided to make it a repo. This is what I had when I made that decision.
    
    catch.hpp is just for unit testing (see Test.cxx).

---
+++ HashTable.cxx
@@ -0,0 +1,305 @@
+#include <string>
+#include <iostream>
+#include <stdio.h>
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Item, Item Node, Item List
+
+template<typename K, typename V>
+struct Item {
+  K key;
+  V value;
+}; // struct Item {
+
+
+
+template<typename K, typename V>
+class Item_Node {
+  private:
+    Item<K, V> Item;
+    Item_Node<K,V>* Next;
+
+  public:
+    // Constructor, destructor
+    Item_Node(const K& key, const V& value): Next(NULL) {
+      Item.key = key;
+      Item.value = value;
+    } // Item_Node(const K& key, const V& value): Next(NULL) {
+    ~Item_Node() {}
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Key, value methods
+
+    // determine key and value of this node
+    V getValue() const { return Item.value; }
+    K getKey() const { return Item.key; }
+
+    // We need to be able to update the item's value (though not the each node's
+    // key should be constant)
+    void setValue(V New_Value) { Item.value = New_Value; }
+
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Next methods
+    Item_Node* getNext() const { return Next; }
+    void setNext(Item_Node<K, V>* Next ) { Item_Node::Next = Next; }
+}; // class Item_Node {
+
+
+
+// Item List exception classes.
+class List_Exception {
+  private:
+    std::string Error_Message;
+  public:
+    List_Exception(const char* Error_Message): Error_Message(Error_Message) {}
+    const char* what() const { return Error_Message.c_str(); }
+}; // class List_Exception {
+
+class Item_Not_In_List: public List_Exception {
+  public:
+    Item_Not_In_List(const char * Error_Message) : List_Exception(Error_Message) {}
+}; // class Item_Not_In_List_Error: public List_Exception {
+
+
+
+// Item list
+template<typename K, typename V>
+class Item_List {
+  private:
+    Item_Node<K,V>* Start;                  // First node in the list
+    Item_Node<K,V>* End;                    // Last node in the list
+
+    /* Since lists deal with dynamic memory, we need to eliminate the default
+    copy constructor and = operator */
+    Item_List(const Item_List<K, V> & ) = delete;
+    Item_List<K, V>& operator=(const Item_List &) = delete;
+
+  public:
+    // Constructors, destructor
+    Item_List() : Start(NULL), End(NULL) {}
+    ~Item_List() {
+      // Starting from Start, cycle through the nodes and free them 1 by 1
+      while(Start != NULL) {
+        Item_Node<K, V>* Next = Start->getNext();
+        delete Start;
+        Start = Next;
+      } // while(Start != End) {
+    } // ~Item_List() {
+
+
+    /* Put a new value in the list. If the new value's key matches an existing
+    item's key then we update that item's value. Otherwise, add a new item
+    to the end of the list. */
+    void put(const K key, const V value) {
+      /* Check if any of the nodes in the list have a key that matches the new
+      key. If so, update that node's value. Otherwise, append a new node to the
+      end of the list */
+      Item_Node<K, V>* entry = Start;
+      while(entry != NULL) {
+        /* Check if the key of the current entry matches the key. If so, update
+        that node's value and return. Otherwise, move onto the next node */
+        if(entry->getKey() == key) {
+          entry->setValue(value);
+          return;
+        } // if(entry->getKey() == key) {
+        else { entry = entry->getNext(); }
+      } // while(entry != Null) {
+
+      /* If we are here then that means that new key did not match the key of
+      any of the existing nodes in this list. That could mean the list is empty,
+      a case that we need to handle. */
+
+      /* If Start == NULL then this is the first node in the list. */
+      if(Start == NULL) {
+        Start = new Item_Node<K, V>{key, value};
+        End = Start;
+      } // if(Start == NULL) {
+
+      /* Otherwise, we need to append a new node onto the end of the existing
+      list */
+      else {
+        Item_Node<K, V>* New_End = new Item_Node<K, V>{key, value};
+        End->setNext(New_End);
+        End = New_End;
+      } // else
+    } // void put(const K key, const V value) {
+
+
+    // Remove an item with a particular key from the list
+    void remove(const K key) {
+      /* Cycle through the nodes. If we find one whose key matches the specified
+      key then remove that item from the list. */
+      Item_Node<K, V>* prev = NULL;
+      Item_Node<K, V>* entry = Start;
+
+      while(entry != NULL) {
+        // If entry's key matches the specified key, remove that node!
+        if(entry->getKey() == key) {
+          /* If the first node's key matches the specified key then we just need
+          to update Start. Otherwise, we need to have prev the previous node
+          point to the node after entry. */
+          if(entry == Start) {
+            /* If the list has just one item, then we need to also update End.
+            Otherwise, just have Start point to the next node. */
+            if(Start == End) { Start = End = NULL; }
+            else { Start = Start->getNext(); }
+          } //   if(entry == Start) {
+          else { prev->setNext(entry->getNext()); }
+
+          // Now delete the removed node
+          delete entry;
+        } // if(entry->getKey() == key) {
+
+        // Otherwise, move onto the next node
+        prev = entry;
+        entry = entry->getNext();
+      } // while(entry != NULL) {
+
+      /* If we get here then that means that the specified key did not match the
+      key of any node in the list. In this case, there is nothing to remove, so
+      we're done */
+      return;
+    } // void remove(const K key) {
+
+
+    // Get the value of the node with a particular key. If no such node is
+    // found, then throw an exception.
+    V get(const K key) {
+      /* Search through the items in the list until we find one whose key matches the
+      specified key. If no such key is found, throw an exception. */
+      Item_Node<K, V>* entry = Start;
+      while(entry != NULL) {
+        // if entry's key matches the specified key they return that node's value
+        if(entry->getKey() == key) { return entry->getValue(); }
+
+        // Otherwise, move onto the next item
+        entry = entry->getNext();
+      } // while(entry != NULL)
+
+      /* If we get here then none of the items in the list had a key that
+      matched the specified key. As such, throw an exception! */
+      char Error_Message_Buffer[500];
+      sprintf(Error_Message_Buffer, "Item Not In List Error: There are no items in this list with key %d\n", key);
+      throw Item_Not_In_List(Error_Message_Buffer);
+    } // V get(const K key) {
+
+
+    // Printing method
+    friend std::ostream& operator<<(std::ostream & os, const Item_List<K, V>& List) {
+      Item_Node<K, V>* entry = List.Start;
+
+      while(entry != NULL) {
+        os << "{" << entry->getKey() << " : " << entry->getValue() << "}";
+
+        // If there is another node to print then indiciate so with an arrow
+        if(entry->getNext() != NULL ) { os << " -> "; }
+
+        entry = entry->getNext();
+      } // while(entry != NULL) {
+
+      return os;
+    } // std::ostream& operator<<(std::ostream & os, const Item_Node<K, V>& List) {
+}; // class Item_List {
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Hash table
+
+// Hash Table exception classes.
+class Hash_Table_Exception {
+  private:
+    std::string Error_Message;
+  public:
+    Hash_Table_Exception(const char* Error_Message): Error_Message(Error_Message) {}
+    const char* what() const { return Error_Message.c_str(); }
+}; // class Hash_Table_Exception {
+
+class Invalid_Key: public Hash_Table_Exception {
+  public:
+    Invalid_Key(const char * Error_Message) : Hash_Table_Exception(Error_Message) {}
+}; // class Invalid_Key: public Hash_Table_Exception {
+
+
+
+template <typename V>
+class Hash_Table {
+  private:
+    unsigned N_Buckets;
+    Item_List<unsigned, V>* Buckets;
+
+    // Hashing function
+    unsigned Hash(unsigned key) const { return (key % N_Buckets); }
+
+    // Delete the implicit = operator and copy constructor methods
+    Hash_Table(const Hash_Table &) = delete;
+    Hash_Table& operator=(const Hash_Table &) = delete;
+
+  public:
+    // Constructor, destructor
+    Hash_Table(unsigned N_Buckets = 16) {
+      /* I require that there are at least 16 buckets (I picked a power of 2)
+      So that the hashing function can work super quickly. */
+      if(N_Buckets < 16) { N_Buckets = 16; }
+
+      Hash_Table::N_Buckets = N_Buckets;
+      Buckets = new Item_List<unsigned, V>[N_Buckets];
+    } // Hash_Table(unsigned N_Buckets = 16) {
+
+    ~Hash_Table() { delete [] Buckets; }
+
+
+    // Insert an item into the table.
+    void insert(unsigned key, V value) {
+      // First, calculate the key of the hash
+      unsigned bucket_index = Hash(key);
+
+      // Now, add the new key-value pair into the selected bucket.
+      Buckets[bucket_index].put(key, value);
+    } // void insert(unsigned key, V value) {
+
+
+    // remove the value with the specified key from the table.
+    void remove(unsigned key) {
+      // Calculate the bucket index.
+      unsigned bucket_index = Hash(key);
+
+      // Remove the item with the specified key from the selected bucket
+      Buckets[bucket_index].remove(key);
+    } // void remove(unsigned key) {
+
+
+    /* Find the value of the item with the specified key. Throws an exception
+    if no item with the specified key can be found */
+    V search(unsigned key) const {
+      // First, find the bucket index.
+      unsigned bucket_index = Hash(key);
+
+      // Now, try finding an item with the specified key in the selected bucket.
+      try { return Buckets[bucket_index].get(key); }
+      catch (const Item_Not_In_List& Er ) {
+        /* If no item with the specified value can be found, then we raise an
+        Invalid_Key exception. */
+        char Error_Message_Buffer[500];
+        sprintf(Error_Message_Buffer,
+                "Invalid Key Error: This hash table does not have an entry with key %d\n",
+                key);
+        throw Invalid_Key(Error_Message_Buffer);
+      } // catch (const Item_Not_In_List& Er ) {
+    } // V search(unsigned key) const {
+
+
+    // Printing method
+    friend std::ostream & operator<<(std::ostream & os, const Hash_Table & Table) {
+      unsigned N_Buckets = Table.N_Buckets;
+      for(unsigned i = 0; i < N_Buckets; i++) {
+        os << "Bucket " << i << ": " << Table.Buckets[i] << std::endl;
+      } // for(unsigned i = 0; i < N_Buckets; i++) {
+
+      return os;
+    } // friend std::ostream & operator<<(std::ostream & os, const Hash_Table & Table) {
+}; // class Hash_Table {
---
+++ Test.cxx
@@ -0,0 +1,122 @@
+#include <cstdlib>
+#include <iostream>
+#include "HashTable_Chaining.cxx"
+
+// Unit testing stuff
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+// Test Item Node
+TEST_CASE("Item Node tests", "[Item_Node]") {
+  // First, make two Item Nodes.
+  unsigned key1 = 1;
+  unsigned key2 = 5;
+  double value1 = 2.2;
+  double value2 = 3.23;
+
+  Item_Node<unsigned, double> One(key1, value1);
+  Item_Node<unsigned, double> Two(key2, value2);
+
+  // Now, check that they set up the values correctly
+  REQUIRE(One.getKey() == key1);
+  REQUIRE(One.getValue() == value1);
+  REQUIRE(Two.getKey() == key2);
+  REQUIRE(Two.getValue() == value2);
+
+  // Now check that item nodes can update their next paramater as expected.
+  REQUIRE(One.getNext() == NULL);
+  One.setNext(&Two);
+  Two.setNext(&One);
+  REQUIRE(One.getNext() == &Two );
+  REQUIRE(Two.getNext() == &One );
+
+  // Now check that Item_Nodes can have their values updated
+  double new_value = 23.232;
+  One.setValue(new_value);
+  REQUIRE(One.getValue() == new_value);
+} // TEST_CASE("Item Node tests", "[Item_Node]") {
+
+
+
+// Test Item List
+TEST_CASE("Item List tests", "[Item_List]") {
+  // let's make an item list.
+  Item_List<unsigned, double> List;
+
+  // Let's search for an item in the list. The list should be empty, so this
+  // should throw an exception
+  REQUIRE_THROWS( List.get(12) );
+
+  /* Add an item, see that it's accessible, remove that item (so that the list
+  is empty again) and check that the item is now inaccessible. */
+  double value0 = 20392.20;
+  List.put(1, value0);
+  REQUIRE( List.get(1) == value0 );
+  List.remove(1);
+  REQUIRE_THROWS( List.get(1) );
+
+  // Now, let's add some items and see that they're accessible
+  double value1 = 2.332;
+  double value2 = -1.293;
+  List.put(12, value1);
+  List.put(14, value2);
+  REQUIRE( List.get(14) == value2 );
+  REQUIRE( List.get(12) == value1 );
+  //REQUIRE_THROWS( List.get(5) );
+
+  // Now, update one of the values and see that it worked
+  double value3 = 29392.32;
+  List.put(12, value3);
+  REQUIRE( List.get(12) == value3 );
+
+  // Now, remove an item and make sure that it's inaccessible
+  List.remove(12);
+  REQUIRE_THROWS( List.get(12) );
+} // TEST_CASE("Item List tests", "[Item_List]") {
+
+
+
+void Print_Table(const Hash_Table<double> & H) { std::cout << H; }
+
+TEST_CASE("Hash Table tests!", "[Hash_Table]") {
+  /* First, let's make a hashtable of doubles. We'll use the default number of
+  buckets for now. */
+  Hash_Table<double> H{};
+
+  /* Let's try searching for some items. Since the table is empty, all of these
+  attempts should throw an exception */
+  for(unsigned i = 0; i < 10; i++) {
+    unsigned random_key = rand();
+    REQUIRE_THROWS( H.search(random_key) );
+  } // for(unsigned i = 0; i < 10; i++) {
+
+  /* Now let's try inserting some items into the table and seeing that the
+  table is able to find them  */
+  unsigned key1 = 52, key2 = 47;
+  double value1 = 1.203, value2 = 29392.392;
+  H.insert(key1, value1);
+  H.insert(key2, value2);
+  REQUIRE( H.search(key1) == value1 );
+  REQUIRE( H.search(key2) == value2 );
+  REQUIRE_THROWS( H.search(0) );
+
+  /* Now let's make sure that the table can handle collissions */
+  unsigned key3 = 0, key4 = 16;
+  double value3 = 129.293, value4 = 5802.29;
+  H.insert(key3, value3);
+  H.insert(key4, value4);
+  REQUIRE( H.search(key3) == value3 );
+  REQUIRE( H.search(key4) == value4 );
+
+  /* Now, test that the table can update a particular key/value pair. */
+  double value5 = 123.456;
+  H.insert(key4, value5);
+  REQUIRE( H.search(key4) == value5 );
+
+  // Just for fun, print out the table!
+  std::cout << H;
+
+  /* Now check that we can remove items from the table */
+  H.remove(key4);
+  REQUIRE_THROWS( H.search(key4) );
+} // TEST_CASE("Hash Table tests!", "[Hash_Table]") {
